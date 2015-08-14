8f6afd6296c07f75c7774130d7c76ea7de8a2547
Curtis Fenner <cfenneremail@gmail.com>
2015-08-14 18:01:44 -0400

    real 16 boot

---
+++ boot.asm
@@ -0,0 +1,19 @@
+; A simple boot sector that prints a message.
+mov ah, 0x0e ; Enter teletype mode for interrupt 0x10
+
+mov al, 'H' ; interrupt 0x10 character argument
+int 0x10
+mov al, 'e' ; interrupt 0x10 character argument
+int 0x10
+mov al, 'l' ; interrupt 0x10 character argument
+int 0x10
+mov al, 'l' ; interrupt 0x10 character argument
+int 0x10
+mov al, 'o' ; interrupt 0x10 character argument
+int 0x10
+
+jmp $
+
+; Padding + magic boot number
+times 510-($-$$) db 0
+dw 0xaa55 ; magic number
